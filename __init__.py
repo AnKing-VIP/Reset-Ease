318ed9d14a7819d2ccc85d36e1f1de4b4c333580
noobj2 <63994740+noobj2@users.noreply.github.com>
2020-05-12 07:39:34 +0430

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,24 +1,46 @@
-# import the main window object (mw) from aqt
 from aqt import mw
-# import the "show info" tool from utils.py
-from aqt.utils import showInfo
-# import all of the Qt GUI library
+from aqt.utils import showInfo, askUser
 from aqt.qt import *
 
-
-ezFactor = 2500
-ezFactor2 = ezFactor/10
-
 def ResetEase():
-    mw.col.db.execute("update cards set factor = ?", ezFactor)
-    # show a message box
-    showInfo("Ease has been reset to " + str(ezFactor2) + "%.")
+    dialog = QDialog(mw)
+    dialog.setWindowFlags(Qt.Dialog | Qt.MSWindowsFixedSizeDialogHint)
+    ease_label = QLabel("Enter Ease: ")
+    ease_spinbox = QSpinBox()
+    ease_spinbox.setRange(130, 500)
+    ease_spinbox.setValue(250)
+    ease_spinbox.setSuffix("%")
+    ease_spinbox.setSingleStep(10)
+    user_ease = ease_spinbox.value()
+    reset_button = QPushButton("&Reset")
+    reset_button.clicked.connect(lambda: accept(user_ease))
+    reset_button.clicked.connect(lambda: dialog.hide())
+    cancel_button = QPushButton("&Cancel")
+    cancel_button.clicked.connect(lambda: dialog.hide())
+    ease_line = QHBoxLayout()
+    ease_line.addWidget(ease_label)
+    ease_line.addWidget(ease_spinbox)
+    button_line = QHBoxLayout()
+    button_line.addWidget(reset_button)
+    button_line.addWidget(cancel_button)
+    layout = QVBoxLayout()
+    layout.addLayout(ease_line)
+    layout.addLayout(button_line)
+    dialog.setLayout(layout)
+    dialog.setWindowTitle("Reset Ease")
+    dialog.show()
+
+def accept(user_ease):
+    anki_ease = user_ease * 10
+    reset = askUser("<div style='font-size: 16px'> Reset all cards Ease to {}%?<br><font color=red>This action can't be undone.</font></div>".format(user_ease), defaultno=True, title="Reset Ease")
+    if reset:
+        mw.col.db.execute("update cards set factor = ?", anki_ease)
+        showInfo("Ease has been reset to {}%.".format(user_ease), title="Reset Ease")
+    else:
+        pass
 
 
-# create a new menu item, "test"
-action = QAction("Reset Ease", mw)
-# set it to call testFunction when it's clicked
+action = QAction("Reset &Ease", mw)
 action.triggered.connect(ResetEase)
-# and add it to the tools menu
 mw.form.menuTools.addAction(action)
-
+mw.addonManager.setConfigAction(__name__, ResetEase)
