88c14b1c06f04f6e49f7803c35544329c1468b8c
noobj2 <63994740+noobj2@users.noreply.github.com>
2020-06-23 07:34:32 +0430

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,45 +2,59 @@ from aqt import mw
 from aqt.utils import showInfo, askUser
 from aqt.qt import *
 
-def ResetEase():
-    dialog = QDialog(mw)
-    dialog.setWindowFlags(Qt.Dialog | Qt.MSWindowsFixedSizeDialogHint)
-    ease_label = QLabel("Enter Ease: ")
-    ease_spinbox = QSpinBox()
-    ease_spinbox.setRange(130, 500)
-    ease_spinbox.setValue(250)
-    ease_spinbox.setSuffix("%")
-    ease_spinbox.setSingleStep(10)
-    user_ease = ease_spinbox.value()
-    reset_button = QPushButton("&Reset")
-    reset_button.clicked.connect(lambda: accept(user_ease))
-    reset_button.clicked.connect(lambda: dialog.hide())
-    cancel_button = QPushButton("&Cancel")
-    cancel_button.clicked.connect(lambda: dialog.hide())
-    ease_line = QHBoxLayout()
-    ease_line.addWidget(ease_label)
-    ease_line.addWidget(ease_spinbox)
-    button_line = QHBoxLayout()
-    button_line.addWidget(reset_button)
-    button_line.addWidget(cancel_button)
-    layout = QVBoxLayout()
-    layout.addLayout(ease_line)
-    layout.addLayout(button_line)
-    dialog.setLayout(layout)
-    dialog.setWindowTitle("Reset Ease")
-    dialog.show()
 
-def accept(user_ease):
-    anki_ease = user_ease * 10
-    reset = askUser("<div style='font-size: 16px'> Reset all cards Ease to {}%?<br><font color=red>This action can't be undone.</font></div>".format(user_ease), defaultno=True, title="Reset Ease")
-    if reset:
-        mw.col.db.execute("update cards set factor = ?", anki_ease)
-        showInfo("Ease has been reset to {}%.".format(user_ease), title="Reset Ease")
-    else:
-        pass
+class ResetEase(QDialog):
+    def __init__(self, parent=mw):
+        super(ResetEase, self).__init__(parent)
+        self.mainWindow()
 
+
+    def mainWindow(self):
+        self.choose_ease()
+        self.setWindowFlags(Qt.Dialog | Qt.MSWindowsFixedSizeDialogHint)
+        self.setLayout(self.layout)
+        self.setWindowTitle("Reset Ease")
+
+
+    def choose_ease(self):
+        ease_label = QLabel("Enter Ease: ")
+        self.ease_spinbox = QSpinBox()
+        self.ease_spinbox.setRange(130, 500)
+        self.ease_spinbox.setValue(250)
+        self.ease_spinbox.setSuffix("%")
+        self.ease_spinbox.setSingleStep(10)
+        reset_button = QPushButton("&Reset")
+        reset_button.clicked.connect(lambda: self.accept())
+        reset_button.clicked.connect(lambda: self.hide())
+        cancel_button = QPushButton("&Cancel")
+        cancel_button.clicked.connect(lambda: self.hide())
+        ease_line = QHBoxLayout()
+        ease_line.addWidget(ease_label)
+        ease_line.addWidget(self.ease_spinbox)
+        button_line = QHBoxLayout()
+        button_line.addWidget(reset_button)
+        button_line.addWidget(cancel_button)
+        self.layout = QVBoxLayout()
+        self.layout.addLayout(ease_line)
+        self.layout.addLayout(button_line)
+
+
+    def accept(self):
+        user_ease = self.ease_spinbox.value()
+        anki_ease = user_ease * 10
+        reset = askUser("<div style='font-size: 16px'> Reset all cards Ease to {}%?<br><font color=red>This action can't be undone.</font></div>".format(user_ease), defaultno=True, title="Reset Ease")
+        if reset:
+            mw.col.db.execute("update cards set factor = ?", anki_ease)
+            showInfo("Ease has been reset to {}%.".format(user_ease), title="Reset Ease")
+        else:
+            pass
+
+
+def open_window():
+    ease_dialog = ResetEase()
+    ease_dialog.exec()
 
 action = QAction("Reset &Ease", mw)
-action.triggered.connect(ResetEase)
+action.triggered.connect(open_window)
 mw.form.menuTools.addAction(action)
-mw.addonManager.setConfigAction(__name__, ResetEase)
+mw.addonManager.setConfigAction(__name__, open_window)
