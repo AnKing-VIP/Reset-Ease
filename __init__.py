611afe4f91c76043a3f56146286beb7a232ef354
r-appleton <58394763+r-appleton@users.noreply.github.com>
2020-12-26 13:37:28 +0000

    Update __init__.py
    - sorted decks alphabetically by name
    - (make deck names accommodate longer deck names)
    - added ability to reset just cards stuck in ease hell (ie. 130%)
    - explain what will happen before user action
    - simplified dialog code (no subsequent popup question required)

--- __init__.py
+++ __init__.py
@@ -2,90 +2,125 @@ from aqt import mw
 from aqt.utils import showInfo, askUser
 from aqt.qt import *
 
+class QHSeparationLine(QFrame):
+    '''
+    a horizontal separation line\n
+    '''
+    def __init__(self):
+        super().__init__()
+        self.setMinimumWidth(1)
+        self.setFixedHeight(20)
+        self.setFrameShape(QFrame.HLine)
+        self.setFrameShadow(QFrame.Sunken)
+        self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Minimum)
 
-class ResetEase(QDialog):
+class DlgResetEase(QDialog):
     def __init__(self, parent=mw):
-        super(ResetEase, self).__init__(parent)
-        self.mainWindow()
-
-
-    def mainWindow(self):
-        self.choose_ease()
-        self.setWindowFlags(Qt.Dialog | Qt.MSWindowsFixedSizeDialogHint)
-        self.setLayout(self.layout)
+        super(DlgResetEase, self).__init__(parent)
         self.setWindowTitle("Reset Ease")
+        self.setWindowFlags(Qt.Dialog | Qt.MSWindowsFixedSizeDialogHint)
 
+        self._deck_chooser = QComboBox()
+        self._deck_chooser.addItem('Whole Collection', None)
+        decks = {item["name"]:item["id"] for item in mw.col.decks.all()}
+        for name in sorted(decks.keys()):
+            self._deck_chooser.addItem(name, decks[name])
+        self._deck_chooser.activated.connect(self._describe)
 
-    def choose_ease(self):
-        deck_label = QLabel("Deck: ")
-        deck_label.setFixedWidth(60)
-        self.deck_chooser = QComboBox()
-        self.deck_chooser.setFixedWidth(200)
-        self.deck_chooser.addItem('Whole Collection', 'collection')
-        did_list = []
-        decks = mw.col.decks.all()
-        for item in decks:
-            deck_name = item["name"]
-            deck_id = item["id"]
-            self.deck_chooser.addItem(deck_name, deck_id)
-            did_list.append(deck_id)
-        ease_label = QLabel("Ease: ")
-        ease_label.setFixedWidth(60)
-        self.ease_spinbox = QSpinBox()
-        self.ease_spinbox.setFixedWidth(200)
-        self.ease_spinbox.setRange(130, 500)
-        self.ease_spinbox.setValue(250)
-        self.ease_spinbox.setSuffix("%")
-        self.ease_spinbox.setSingleStep(10)
-        reset_button = QPushButton("&Reset")
-        reset_button.clicked.connect(lambda: self.accept())
-        reset_button.clicked.connect(lambda: self.hide())
-        cancel_button = QPushButton("&Cancel")
-        cancel_button.clicked.connect(lambda: self.hide())
-        deck_line = QHBoxLayout()
-        deck_line.addWidget(deck_label)
-        deck_line.addWidget(self.deck_chooser)
-        ease_line = QHBoxLayout()
-        ease_line.addWidget(ease_label)
-        ease_line.addWidget(self.ease_spinbox)
-        button_line = QHBoxLayout()
-        button_line.addWidget(reset_button)
-        button_line.addWidget(cancel_button)
-        self.layout = QVBoxLayout()
-        self.layout.addLayout(deck_line)
-        self.layout.addLayout(ease_line)
-        self.layout.addLayout(button_line)
+        self._new_ease = self._spinbox(250, '250% is the default Anki starting ease')
+        self._old_ease = self._spinbox(130, '130% is the value for cards stuck in Anki "ease hell"')
+        self._any_old_ease = QCheckBox()
+        self._any_old_ease.setText('any')
+        self._any_old_ease.toggled.connect(self._toggled)
+ 
+        self._explanation = QLabel()
+        self._explanation.setWordWrap(True)
+        self._describe()
 
+        buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+        buttonBox.accepted.connect(self.accept)
+        buttonBox.rejected.connect(self.reject)
 
-    def accept(self):
-        deck_id = self.deck_chooser.currentData()
-        deck_name = self.deck_chooser.currentText()
-        user_ease = self.ease_spinbox.value()
-        anki_ease = user_ease * 10
-        reset = askUser("<div style='font-size: 16px'>Reset ease for all cards in \"{}\" to {}%?<br><font color=red>This action can't be undone.</font></div>".format(deck_name, user_ease), defaultno=True, title="Reset Ease")
-        if reset:
-            # Fetch all cards to update.
-            if deck_name == "Whole Collection":
-                card_ids = mw.col.db.list("SELECT id FROM cards WHERE factor != 0")
-            else:
-                card_ids = mw.col.db.list("SELECT id FROM cards WHERE factor != 0 AND did = ?", deck_id)
-            # Update and flush the cards so on sync they will be gracefully updated.
-            num_cards = 0
-            for card_id in card_ids:
-                card = mw.col.getCard(card_id)
-                # Don't touch cards which already have the requested ease.
-                if card.factor != anki_ease:
-                    card.factor = anki_ease
-                    card.flush()
-                    num_cards += 1
-            showInfo("Ease factor of {} card{} has been reset to {}%.".format(num_cards, "s" if num_cards != 1 else "", user_ease), title="Reset Ease")
+        layout = QGridLayout()
+        layout.addWidget(self._label('Deck: '), 0, 0)
+        layout.addWidget(self._deck_chooser, 0, 1, 1, 2)
+        layout.addWidget(self._label('Old ease: '), 1, 0)
+        layout.addWidget(self._old_ease, 1, 1)
+        layout.addWidget(self._any_old_ease, 1, 2)
+        layout.addWidget(self._label('New ease: '), 2, 0)
+        layout.addWidget(self._new_ease, 2, 1)
+        layout.addWidget(QHSeparationLine(), 3, 0, 1, 3)
+        layout.addWidget(self._explanation, 4, 0, 1, 3)
+        layout.addWidget(QLabel('<font color=red>This action cannot be undone.</font>'), 5, 0, 1, 3)
+        layout.addWidget(QHSeparationLine(), 6, 0, 1, 3)
+        layout.addWidget(buttonBox, 7, 0, 1, 3)
+        self.setLayout(layout)
+
+    def _spinbox(self, value, tooltip):
+        spinbox = QSpinBox()
+        spinbox.setRange(130, 500)
+        spinbox.setValue(value)
+        spinbox.setSuffix("%")
+        spinbox.setSingleStep(10)
+        spinbox.setToolTip(tooltip)
+        spinbox.valueChanged.connect(self._describe)
+        return spinbox
+
+    def _label(self, text):
+        label = QLabel(text)
+        label.setFixedWidth(90)
+        return label
+
+    def _toggled(self):
+        self._old_ease.setEnabled(not self._any_old_ease.isChecked())
+        self._describe()
+
+    def _describe(self):
+        (sql, params) = self.sql('count(*)')
+        n = mw.col.db.scalar(sql, *params)
+        d = 'the {0} deck'.format(self._deck_chooser.currentText()) if self._deck_chooser.currentData() else 'any deck' 
+        e = 'a different ease' if self._any_old_ease.isChecked() else 'an ease of {0}%'.format(self._old_ease.value())
+        s = 'Press OK to change the ease to {0}% for the {1} cards in {2} which currently have {3}.\n'.format(self.new_ease(), n, d, e)
+        self._explanation.setText(s)
+
+    def new_ease(self):
+        return self._new_ease.value()
+
+    def sql(self, what='id'):
+        predicates = []
+        parameters = []
+    
+        # don't touch cards which already have the requested new ease.
+        predicates.append('factor != ?')
+        parameters.append(self._new_ease.value() * 10)
+    
+        if self._any_old_ease.isChecked():
+            predicates.append('factor != 0')
         else:
-            pass
+            predicates.append('factor = ?')
+            parameters.append(self._old_ease.value() * 10)
+    
+        if self._deck_chooser.currentData() is not None:
+            predicates.append('did = ?')
+            parameters.append(self._deck_chooser.currentData())
+    
+        select = 'SELECT {0} FROM cards'.format(what)
+        return (select + ' WHERE ' + ' AND '.join(predicates), parameters)
 
 
 def open_window():
-    ease_dialog = ResetEase()
-    ease_dialog.exec()
+    ease_dialog = DlgResetEase()
+    if ease_dialog.exec():
+        (sql, params) = ease_dialog.sql()
+        num_cards = 0
+        # update and flush the cards so on sync they will be gracefully updated.
+        for card_id in mw.col.db.list(sql, *params):
+            card = mw.col.getCard(card_id)
+            card.factor = ease_dialog.new_ease() * 10
+            card.flush()
+            num_cards += 1
+        msg = "Ease factor of {} card{} has been reset to {}%.".format(num_cards, "s" if num_cards != 1 else "", ease_dialog.new_ease())
+        showInfo(msg, title="Reset Ease")
 
 action = QAction("Reset &Ease", mw)
 action.triggered.connect(open_window)
